dc04d911939c1fab67ccbd4ee0e5dc1cc5e19ea6
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-12-08 09:26:32 +0900

    Admin LoginScreen 구현

---
+++ KioskSystrm/src/main/java/com/crenu/kiosk/ui/LoginScreen.java
@@ -0,0 +1,42 @@
+package com.crenu.kiosk.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static com.crenu.kiosk.KioskSystem.uiManager;
+import static com.crenu.kiosk.ui.PanelNameEntity.LOGIN_PANELNAME;
+
+public class LoginScreen extends JPanel {
+
+    public LoginScreen() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JButton btnLogin = new JButton("Login");
+        JTextField userIDField = new JTextField(20);
+        JPasswordField passwordField = new JPasswordField(20);
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCredentials(userIDField.getText(), String.valueOf(passwordField.getPassword()));
+            }
+        });
+
+        add(userIDField);
+        add(passwordField);
+        add(btnLogin);
+    }
+
+    private void checkCredentials(String userID, String password) {
+        String adminID = "admin";
+        String adminPassword = "password";
+
+        if(userID.equals(adminID) && password.equals(adminPassword)) {
+            JOptionPane.showMessageDialog(null, "로그인 성공");
+        } else {
+            JOptionPane.showMessageDialog(null, "Invalid credentials. Please try again.");
+        }
+    }
+}
--- KioskSystrm/src/main/java/com/crenu/kiosk/ui/PanelNameEntity.java
+++ KioskSystrm/src/main/java/com/crenu/kiosk/ui/PanelNameEntity.java
@@ -2,6 +2,6 @@ package com.crenu.kiosk.ui;
 
 public class PanelNameEntity {
     public static String INITAL_PANELNAME = "Inital";
-
     public static String MENU_PANELNAME = "Menu Display";
+    public static String LOGIN_PANELNAME = "Login";
 }
